885c85317c4f4721b7994e4328e71690a48232d8
Aakash106848 <aakash.b@gavstech.com>
2023-10-30 10:18:28 +0530

    first commit

---
+++ src/app/UserData.ts
@@ -0,0 +1,9 @@
+export class UserData   {
+    userslist:Array<object>=[];
+    FirstName:string = 'Aakash';
+    LastName:string = 'B';
+    Location:string = 'Chennai';
+    Company:string = 'Gavs';
+    salary:number = 5404;
+    dateofjoining:Date = new Date();
+}
